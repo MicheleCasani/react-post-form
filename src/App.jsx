63d3c717800f043adeee08d320c8ff9fcf0cfca8
MicheleCasani <casanimichele@gmail.com>
2025-05-14 15:04:36 +0200

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
-
+import axios from 'axios'
+import PostForm from './PostForm'
 
 function App() {
 
 
   return (
     <>
-
+      <div className='container'>
+        <div className="row">
+          <div className="col-12">
+            <h1>Form</h1>
+          </div>
+        </div>
+        <PostForm />
+      </div>
     </>
   )
 }
---
+++ src/PostForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+const TicketForm = () => {
+    const [formData, setFormData] = useState({
+        title: "",
+        author: "",
+        body: "",
+        public: false,
+        id: ""
+    })
+}
+
+function PostForm() {
+    return (
+        <form>
+            <div className="row g-3">
+                <div className="col-12 col-md-6">
+                    <input
+                        type="text"
+                        placeholder="Autore"
+                        className="form-control"
+                    />
+                </div>
+                <div className="col-12 col-md-6">
+                    <input
+                        type="text"
+                        placeholder="Titolo"
+                        className="form-control"
+                    />
+                </div>
+                <div className="col-12 ">
+                    <input
+                        type="text"
+                        placeholder="Corpo del post "
+                        className="form-control textarea-corpo"
+                    />
+                </div>
+
+            </div>
+            <div className="col-12 col-md-6 form-check mt-4">
+                Pubblica
+                <input
+                    type="checkbox"
+                    name='Pubblica'
+                    className='mx-2' />
+            </div>
+            <button
+                type="button"
+                className="btn btn-primary mt-4"
+                onClick={''}>
+                Invia
+            </button>
+
+        </form >
+    )
+}
+
+export default PostForm
